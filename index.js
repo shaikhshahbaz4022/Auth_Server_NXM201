338ae0a528ca713b01c90eab7be5c4cfa9ed4fcf
unknown <shaikhshahbaz4022@gmail.com>
2023-05-08 16:25:10 +0530

    Backend_NXM201

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const connection = require('./connection/connection');
+const cors = require("cors");
+const userRouter = require('./routes/userRoute');
+const postRouter = require('./routes/postRoute');
+require("dotenv").config()
+const auth = require('./middleware/auth');
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+app.use("/user",userRouter)
+
+
+
+app.use("/",postRouter)
+
+app.listen(process.env.port, async () => {
+    try {
+        await connection
+        console.log("Connected to Database succesfully");
+    } catch (error) {
+        console.log(error);
+        console.log("Some error while connicting to DB");
+    }
+    console.log(`server is connected to port no ${process.env.port} `);
+})
